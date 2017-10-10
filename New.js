e1c866f6a8769e05ca32806246d1d66340033c9c
ChocolateWarrior <31725029+ChocolateWarrior@users.noreply.github.com>
2017-10-10 15:08:36 +0300

    Add files via upload

---
+++ New.js
@@ -0,0 +1,21 @@
+'use strict';
+
+console.log('Hello, dear ' + prompt'Enter your name');
+//remember name
+
+const number1 = 10;
+const number2 = number1 * 2;
+console.log(number2);
+
+let a = 10;
+let b = 12;
+
+function f1(a) {
+  return(a+5);
+};
+
+ const sum = function f2(a, b) {
+   return(a + b);
+ };
+
+const f1 = number2 =>
